6f2069e3b9c7c607e0dbb12dc057869e329566e5
Jefferson Solano <solanojefferson@gmail.com>
2021-06-28 14:07:33 +1000

    fixed bugs layouts

--- screens/Product.js
+++ screens/Product.js
@@ -281,7 +281,7 @@ const styles = StyleSheet.create({
     bottom: theme.SIZES.BASE,
     left: 0,
     right: 0,
-    bottom: height / 28
+    bottom: height / 20
   },
   quantityButtons: {
     width: 40,
