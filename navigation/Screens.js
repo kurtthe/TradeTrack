d90b9e775520cb67e8613867be6c2f4cff874ded
kurtthe <solanojefferson@gmail.com>
2021-07-03 07:46:19 +1000

    organized item

--- navigation/Screens.js
+++ navigation/Screens.js
@@ -5,7 +5,6 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
-import { Icon } from "../components";
 import { nowTheme } from "../constants";
 
 // screens
@@ -16,40 +15,26 @@ import Help from '../screens/THelp';
 import ForgotPassword from '../screens/TForgotPassword';
 import ChangePassword from '../screens/TChangePassword';
 import PasswordBeenChange from '../screens/TPasswordBeenChange';
+
+
 import Products from '../screens/TProduts';
-
-
-
 import Profile from '../screens/Profile';
 import Register from '../screens/Register';
-import Components from '../screens/Components';
 import Articles from '../screens/Articles';
-import Trending from '../screens/Trending';
-import Fashion from '../screens/Fashion';
 import Category from '../screens/Category';
 import Product from "../screens/Product";
-import Gallery from "../screens/Gallery";
-import NotificationsScreen from "../screens/Notifications";
-import Agreement from "../screens/Agreement";
-import About from "../screens/About";
-import Privacy from "../screens/Privacy";
-import Chat from "../screens/Chat";
 import Cart from "../screens/Cart";
 import Search from '../screens/Search';
-import SettingsScreen from '../screens/Settings';
-
-import PersonalNotifications from "../screens/PersonalNotifications";
-import SystemNotifications from "../screens/SystemNotifications";
-
-// drawer
-import CustomDrawerContent from './Menu';
-
-// header for screens
-import Header from '../components/Header';
-import tabs from "../constants/tabs";
 import PlaceOrders from '../screens/PlaceOrders';
 import orderPlaced from '../screens/OrderPlaced';
 import Example from '../screens/DatePicker';
+
+
+
+// header for screens
+import Header from '../components/Header';
+
+
 
 
 
@@ -61,44 +46,7 @@ const Tab = createBottomTabNavigator();
 const MainTab = createBottomTabNavigator();
 
 
-function NotificationsStack(props) {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color }) => {
-          let iconName;
-          if (route.name === "Personal") {
-            iconName = "single";
-          } else if (route.name === "System") {
-            iconName = "paper";
-          }
-          // You can return any component that you like here!
-          return (
-            <Icon
-              name={iconName}
-              family="NowExtra"
-              size={22}
-              color={color}
-              style={{ marginTop: 10 }}
-            />
-          );
-        }
-      })}
-      tabBarOptions={{
-        activeTintColor: nowTheme.COLORS.PRIMARY,
-        inactiveTintColor: nowTheme.COLORS.TIME,
-        labelStyle: {
-          fontFamily: "montserrat-regular"
-        },
-        style: { borderTopWidth: '0px', paddingHorizonal: 35 }
-      }}
-    >
-      <Tab.Screen name="Personal" component={PersonalNotifications} />
-      <Tab.Screen name="System" component={SystemNotifications} />
-      <Tab.Screen name="Systssssem" component={SystemNotifications} />
-    </Tab.Navigator>
-  );
-}
+
 
 function ProductsStack(props) {
   return (
@@ -113,111 +61,36 @@ function ProductsStack(props) {
           cardStyle: { backgroundColor: "#FFFFFF" }
         }}
       />
+
+      <Stack.Screen
+        name="Category"
+        component={Category}
+        options={{
+          header: ({ navigation, scene }) => {
+            const { params } = scene.descriptor;
+            const title = (params && params.headerTitle) || "Category";
+            return (<Header title={title} back navigation={navigation} scene={scene}  />);
+          },
+          cardStyle: { backgroundColor: "#FFFFFF" }
+        }}
+      />
+      <Stack.Screen
+        name="Product"
+        component={Product}
+        options={{
+          header: ({ navigation, scene }) => {
+            const { params } = scene.descriptor;
+            const title = (params && params.headerTitle) || "Product";
+            return (<Header title={title} back navigation={navigation} scene={scene} />);
+          },
+          cardStyle: { backgroundColor: "#FFFFFF" }
+        }}
+      />
     </Stack.Navigator>
   );
 }
 
-function SettingsStack(props) {
-  return (
-    <Stack.Navigator mode="card" headerMode="screen">
-      <Stack.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header title="Settings" scene={scene} navigation={navigation} />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="Agreement"
-        component={Agreement}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              title="Agreement"
-              scene={scene}
-              navigation={navigation}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="Privacy"
-        component={Privacy}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              title="Privacy"
-              scene={scene}
-              navigation={navigation}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="About"
-        component={About}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header back title="About" scene={scene} navigation={navigation} />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="NotificationsSettings"
-        component={NotificationsScreen}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              title="Notifications"
-              scene={scene}
-              navigation={navigation}
-            />
-          ),
-          cardStyle: { backgroundColor: "#F8F9FE" }
-        }}
-      />
-      <Stack.Screen
-        name="Cart"
-        component={Cart}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              title="Shopping Cart"
-              scene={scene}
-              navigation={navigation}
-            />
-          ),
-          cardStyle: { backgroundColor: "#F8F9FE" }
-        }}
-      />
-      <Stack.Screen
-        name="Notifications"
-        component={NotificationsStack}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              title="Notifications"
-              scene={scene}
-              navigation={navigation}
-            />
-          ),
-          cardStyle: { backgroundColor: "#F8F9FE" }
-        }}
-      />
-    </Stack.Navigator>
-  );
-}
+
 
 function CartStack(props) {
   return (
@@ -262,54 +135,24 @@ function CartStack(props) {
           cardStyle: { backgroundColor: "#FFFFFF" }
         }}
       />
-      <Stack.Screen
-        name="Gallery"
-        component={Gallery}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              transparent
-              white
-              title=""
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          headerTransparent: true
-        }}
-      />
-            <Stack.Screen
-        name="Chat"
-        component={Chat}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title="Rachel Brown"
-              back
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
+      
+           
     </Stack.Navigator>
   );
 }
 
-function ProfileStack(props) {
+function AccountStack(props) {
   return (
-    <Stack.Navigator initialRouteName="Profile" mode="card" headerMode="screen">
+    <Stack.Navigator initialRouteName="Account" mode="card" headerMode="screen">
       <Stack.Screen
-        name="Profile"
+        name="Account"
         component={Profile}
         options={{
           header: ({ navigation, scene }) => (
             <Header
               transparent
               white
-              title="Profile"
+              title="Account"
               navigation={navigation}
               scene={scene}
             />
@@ -318,41 +161,12 @@ function ProfileStack(props) {
           headerTransparent: true
         }}
       />
-      <Stack.Screen
-        name="Cart"
-        component={Cart}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              title="Shopping Cart"
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="Notifications"
-        component={NotificationsStack}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              title="Notifications"
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
+      
     </Stack.Navigator>
   );
 }
 
-function AccountStack(props) {
+function TradeTrakStack(props) {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Account" component={Register} options={{
@@ -391,111 +205,9 @@ function HomeStack(props) {
           cardStyle: { backgroundColor: "#FFFFFF" }
         }}
       />
-      <Stack.Screen
-        name="Trending"
-        component={Trending}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title="Trending"
-              back
-              tabs={tabs.trending}
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="Category"
-        component={Category}
-        options={{
-          header: ({ navigation, scene }) => {
-            const { params } = scene.descriptor;
-            const title = (params && params.headerTitle) || "Category";
-            return (<Header title={title} back navigation={navigation} scene={scene} />);
-          },
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="Product"
-        component={Product}
-        options={{
-          header: ({ navigation, scene }) => {
-            const { params } = scene.descriptor;
-            const title = (params && params.headerTitle) || "Product";
-            return (<Header title={title} back navigation={navigation} scene={scene} />);
-          },
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="Fashion"
-        component={Fashion}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title="Fashion"
-              back
-              tabs={tabs.fashion}
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      {/* <Stack.Screen
-        name="Product"
-        component={Product}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title=""
-              back
-              black
-              transparent
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          headerTransparent: true
-        }}
-      /> */}
-      <Stack.Screen
-        name="Gallery"
-        component={Gallery}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              back
-              transparent
-              white
-              title=""
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          headerTransparent: true
-        }}
-      />
-      <Stack.Screen
-        name="Chat"
-        component={Chat}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title="Rachel Brown"
-              back
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
+     
+    
+    
       <Stack.Screen
         name="Search"
         component={Search}
@@ -506,36 +218,8 @@ function HomeStack(props) {
           cardStyle: { backgroundColor: "#FFFFFF" }
         }}
       />
-      <Stack.Screen
-        name="Cart2"
-        component={Cart}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title="Cart List"
-              back
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="Notifications"
-        component={NotificationsStack}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title="Notifications"
-              back
-              navigation={navigation}
-              scene={scene}
-            />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
+    
+      
     </Stack.Navigator>
   );
 }
@@ -567,22 +251,23 @@ function AppStack(props) {
               tabBarIcon: ({ color, size }) => (
                 <Ionicons name="file-tray-stacked" color={color} size={size} />
               ),
-              //tabBarBadge: 3,
+             
             }}
           />
           <MainTab.Screen
             name="Cart"
             component={CartStack}
             options={{
-              tabBarLabel: 'Cart List',
+              tabBarLabel: 'Cart',
               tabBarIcon: ({ color, size }) => (
                 <Ionicons name="cart" color={color} size={size} />
               ),
+               tabBarBadge: 7,
             }}
           />
           <MainTab.Screen
             name="Account"
-            component={ProfileStack}
+            component={AccountStack}
             options={{
               tabBarLabel: 'Account',
               tabBarIcon: ({ color, size }) => (
@@ -594,10 +279,10 @@ function AppStack(props) {
             }}
           />
           <MainTab.Screen
-            name="Manages Job"
-            component={AccountStack}
+            name="Job Management"
+            component={TradeTrakStack}
             options={{
-              tabBarLabel: 'Manages Job',
+              tabBarLabel: 'Trade Trak',
               tabBarIcon: ({ color, size }) => (
                 <MaterialIcons
                 name="business-center" color={color} size={size}
