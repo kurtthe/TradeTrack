c8ede3afee5d866043189324d7a3152ae7710703
Jefferson Solano <solanojefferson@gmail.com>
2021-08-03 21:19:39 -0500

    sturcture

--- app/navigation/Screens.js
+++ app/navigation/Screens.js
@@ -41,57 +41,7 @@ const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 const MainTab = createBottomTabNavigator();
 
-function ProductsStack(props) {
-  return (
-    <Stack.Navigator mode="card" headerMode="screen">
-      <Stack.Screen
-        name="Products"
-        component={Products}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header title="Products" navigation={navigation} scene={scene} />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
 
-      <Stack.Screen
-        name="Category"
-        component={Category}
-        options={{
-          header: ({ navigation, scene }) => {
-            const { headerTitle } = scene.route.params;
-            const title = headerTitle || "Category";
-            return (<Header title={title} back navigation={navigation} scene={scene}  />);
-          },
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-      <Stack.Screen
-        name="Product"
-        component={Product}
-        options={{
-          header: ({ navigation, scene }) => {
-            const { params } = scene.descriptor;
-            const title = (params && params.headerTitle) || "Product";
-            return (<Header title={title} back navigation={navigation} scene={scene} />);
-          },
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-        <Stack.Screen
-        name="Search"
-        component={Search}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header title="Search" back navigation={navigation} scene={scene} />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" }
-        }}
-      />
-    </Stack.Navigator>
-  );
-}
 
 
 
--- app/screens/Search.jsx
+++ app/screens/Search.jsx
@@ -14,7 +14,7 @@ import { Block, Text, theme } from "galio-framework";
 const { width } = Dimensions.get("screen");
 
 import { articles, categories, nowTheme } from "../constants/";
-import { Icon, Card, Input } from "../components/";
+import { Icon, Card, Input } from "../components";
 
 const suggestions = [
   { id: "DJs", title: "Showers", image: categories["DJs"] },
