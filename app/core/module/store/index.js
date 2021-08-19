23b3d15f54481efd3cd2952f19ce57ccb50df00f
Jefferson Solano <solanojefferson@gmail.com>
2021-08-18 20:39:56 -0500

    details invoices

--- app/core/module/store/index.js
+++ app/core/module/store/index.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import loginReducer from './auth/reducers/login'
 import liveBalanceReducer from './balance/liveBalance'
 import invoicesReducer from './balance/invoices'
+import newsReducer from './news/news'
 
 export const store = configureStore({
   reducer: {
     loginReducer,
     liveBalanceReducer,
-    invoicesReducer
+    invoicesReducer,
+    newsReducer
   },
 });
--- app/shared/dictionaries/end-points.js
+++ app/shared/dictionaries/end-points.js
@@ -4,5 +4,7 @@ export const endPoints = {
   auth: `${environment.api}login`,
   resetPassword: `${environment.api}reset-password`,
   burdensBalance: `${environment.api}burdens/balance`,
-  invoices: `${environment.api}burdens/invoices`
+  invoices: `${environment.api}burdens/invoices`,
+  invoicesDetail: `${environment.api}burdens/invoices/:id?expand=structure`,
+  news: `${environment.api}news`,
 }
