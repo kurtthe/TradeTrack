cea249a411640a2627ad51fedac530d8b3478a55
Jefferson Solano <solanojefferson@gmail.com>
2021-08-18 22:47:46 -0500

    ready download

--- app/core/services/download-file.service.js
+++ app/core/services/download-file.service.js
@@ -1,4 +1,5 @@
 import * as FileSystem from 'expo-file-system';
+import * as Sharing from "expo-sharing";
 
 export class DownloadFile {
   static instance;
@@ -10,20 +11,21 @@ export class DownloadFile {
     return DownloadFile.instance;
   }
 
-  createDownload(url, format, callback) {
+  createDownload(url, format) {
     return FileSystem.createDownloadResumable(
       url,
-      `FileSystem.documentDirectory${format}`,
-      {},
-      callback
+      `${FileSystem.documentDirectory}file.${format}`
     );
   }
 
   async download(url, format){
     try {
-      const downloadResumable = this.createDownload(url,format, this.progressDownload);
+      const downloadResumable = this.createDownload(url,format);
       const { uri } = await downloadResumable.downloadAsync();
-      console.log('Finished downloading to ', uri);
+      if(await Sharing.isAvailableAsync()){
+        await Sharing.shareAsync(uri);
+      }
+
     } catch (e) {
       console.log("error download",e);
     }
