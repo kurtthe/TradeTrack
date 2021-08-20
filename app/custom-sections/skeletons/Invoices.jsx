fa958deb74d6ab116a0036d562a3954873669a85
Mateo Nieto <matheo.developer@gmail.com>
2021-08-19 19:38:51 -0500

    add skeleton

---
+++ app/custom-sections/skeletons/Invoices.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import SkeletonContent from 'react-native-skeleton-content';
+import { StyleSheet } from 'react-native';
+import { Block } from 'galio-framework';
+import { nowTheme } from '@constants';
+
+const firstLayout = [
+  {
+    width: 300,
+    height: 30,
+    marginBottom: 5
+  },
+  {
+    width: 60,
+    height: 20,
+    marginBottom: 5
+  },
+  {
+    width: 350,
+    height: 30,
+  },
+];
+const Invoices = () => {
+  return (
+    <Block style={styles.container}>
+      <SkeletonContent
+        containerStyle={{ flex: 1, width: 300 }}
+        animationDirection="horizontalLeft"
+        boneColor="#ECECEC"
+        layout={firstLayout}
+        isLoading={true}
+      />
+    </Block>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: nowTheme.COLORS.WHITE,
+    shadowColor: nowTheme.COLORS.BLACK,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 3,
+    shadowOpacity: 0.1,
+    elevation: 2,
+    zIndex: 2,
+    height: 'auto',
+    borderRadius: 3,
+    marginBottom: 5,
+  },
+});
+export default Invoices;
