32ef452ff4b78a21cf04a44b81a76cff0f685c99
Jefferson Solano <solanojefferson@gmail.com>
2021-08-19 23:17:51 -0400

    add products to cart done

--- app/core/module/store/index.js
+++ app/core/module/store/index.js
@@ -4,12 +4,14 @@ import loginReducer from './auth/reducers/login'
 import liveBalanceReducer from './balance/liveBalance'
 import invoicesReducer from './balance/invoices'
 import newsReducer from './news/news'
+import productsReducer from './cart/cart'
 
 export const store = configureStore({
   reducer: {
     loginReducer,
     liveBalanceReducer,
     invoicesReducer,
-    newsReducer
+    newsReducer,
+    productsReducer
   },
 });
