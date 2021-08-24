8276b6861b9d1e6a5f81c84b10bc2c48df14245c
Jefferson Solano <solanojefferson@gmail.com>
2021-08-24 16:25:49 -0500

    portal modal bottom

--- app/custom-elements/WebView.jsx
+++ app/custom-elements/WebView.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
-import { StyleSheet, View } from 'react-native';
 import Loading from '@custom-elements/Loading';
-import { Portal } from 'react-native-paper';
 
 const WebViewComponent = (props) => {
   if (!props.show) {
@@ -10,34 +8,13 @@ const WebViewComponent = (props) => {
   }
 
   return (
-    <Portal>
-      <View style={styles.webViewContainer}>
-        <View style={styles.container}>
-          <View style={styles.header}></View>
-          <View style={styles.body}>
-            <WebView
-              source={{ uri: props.url }}
-              startInLoadingState={true}
-              renderLoading={() => <Loading />}
-            />
-          </View>
-        </View>
-      </View>
-    </Portal>
+    <WebView
+      source={{ uri: props.url }}
+      startInLoadingState={true}
+      renderLoading={() => <Loading />}
+    />
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
-    height: '85%',
-  },
-  header: {},
-  webViewContainer: {
-    flex: 1,
-    position: 'relative',
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-  },
-});
+
 
 export default WebViewComponent;
