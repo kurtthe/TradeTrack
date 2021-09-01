f7f89610036b731406b608712668a679db8f3241
Mateo Nieto <matheo.developer@gmail.com>
2021-08-31 22:07:31 -0500

    loadmoreData component

--- app/custom-sections/ListData.jsx
+++ app/custom-sections/ListData.jsx
@@ -1,11 +1,10 @@
-import React, {cloneElement} from 'react';
+import React, { cloneElement } from 'react';
 import { StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { Block, theme } from 'galio-framework';
 import { GetDataPetitionService } from '@core/services/get-data-petition.service';
 import { Button } from '@components';
 
 const { width } = Dimensions.get('screen');
-
 
 class ListData extends React.Component {
   constructor(props) {
@@ -30,36 +29,40 @@ class ListData extends React.Component {
   }
 
   getPetitionData = async () => {
-    if(!this.props.endpoint){
+    if (!this.props.endpoint) {
       return;
     }
-    await this.getDataPetition.getInfo(this.props.endpoint, this.props.actionData, this.state.perPageData);
+
+    await this.getDataPetition.getInfo(this.props.endpoint, this.loadData, this.state.perPageData);
   };
 
-  handleLoadMore(){
+  loadData = (data) => {
+    this.setState({
+      data: data,
+    });
+
+    this.props.actionData && this.props.actionData(data);
+  };
+
+  handleLoadMore() {
     const oldData = this.state.perPageData;
     this.setState({ perPageData: oldData + 20 });
   }
 
   render() {
+    const { children } = this.props;
 
-    const {children} = this.props;
-    
     return (
       <ScrollView>
-        <Block flex>
-          <Block center style={{ paddingVertical: 5 }}>
-            {cloneElement(children, { data:this.state.data})}
-            <Button
-              onPress={() => this.handleLoadMore()}
-              color="info"
-              textStyle={{ fontFamily: 'montserrat-bold', fontSize: 16 }}
-              style={styles.button}
-            >
-              Load More...
-            </Button>
-          </Block>
-        </Block>
+        {cloneElement(children, { data: this.state.data })}
+        <Button
+          onPress={() => this.handleLoadMore()}
+          color="info"
+          textStyle={{ fontFamily: 'montserrat-bold', fontSize: 16 }}
+          style={styles.button}
+        >
+          Load More...
+        </Button>
       </ScrollView>
     );
   }
