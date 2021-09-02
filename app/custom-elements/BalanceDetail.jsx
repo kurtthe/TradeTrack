1d48c7090378583ab0e85facbeb8d24f31ee7b6c
Jefferson Solano <solanojefferson@gmail.com>
2021-09-01 20:34:46 -0500

    balanceDetail empty data

--- app/custom-elements/BalanceDetail.jsx
+++ app/custom-elements/BalanceDetail.jsx
@@ -6,14 +6,20 @@ import { FormatMoneyService } from '@core/services/format-money.service';
 
 const formatMoney = FormatMoneyService.getInstance();
 
+
 const BalanceDetail = (props) => {
+
+  const getValue = ()=>{
+    return props.item.date ? props.item.value : formatMoney.format(props.item.value)
+  }
+  
   return (
     <Block row style={{ justifyContent: 'space-between', paddingTop: 20 }}>
       <Text style={styles.receiptText}>{props.item.title}</Text>
       <Text
         style={[styles.receiptPrice, props.item.color < 0 && { color: nowTheme.COLORS.ORANGE }]}
       >
-        {props.item.date ? props.item.value : formatMoney.format(props.item.value)}
+        {getValue()|| 'empty'}
       </Text>
     </Block>
   );
