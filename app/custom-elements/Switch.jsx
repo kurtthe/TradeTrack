f89fa4a78a7795853c2cfaf6893e255f89262135
Jefferson Solano <solanojefferson@gmail.com>
2021-10-04 22:25:17 -0500

    fix add the same product

--- app/custom-elements/Switch.jsx
+++ app/custom-elements/Switch.jsx
@@ -4,7 +4,7 @@ import { Switch, Platform, StyleSheet } from 'react-native';
 import { nowTheme } from '@constants';
 
 const SwitchComponent = (props) => {
-  const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(true);
 
   const handleChange = () => {
     setIsChecked(!isChecked);
