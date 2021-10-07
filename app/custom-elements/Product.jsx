ce827b45ffe1e6d00f5965264102d2f7c0c6c8ee
kurtthe <solanojefferson@gmail.com>
2021-10-08 09:48:52 +1100

    add  </TouchableWithoutFeedback>

--- app/custom-elements/Product.jsx
+++ app/custom-elements/Product.jsx
@@ -62,6 +62,10 @@ const Product = (props) => {
         <TouchableWithoutFeedback onPress={() => onProductPressed(props.product)}>
           <Image resizeMode="contain" style={styles.image} source={{ uri: props.product.image }} />
         </TouchableWithoutFeedback>
+
+        <TouchableWithoutFeedback onPress={() => onProductPressed(props.product)}>
+         
+       
         <Block flex space="between" style={{ paddingBottom: 7 }}>
           <Block row>
             <Text color={nowTheme.COLORS.LIGHTGRAY} size={sizeConstant}>
@@ -104,6 +108,7 @@ const Product = (props) => {
             )}
           </Block>
         </Block>
+        </TouchableWithoutFeedback>
         <Block center>
           <Button
             color="warning"
