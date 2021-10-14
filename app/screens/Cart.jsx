fb4820d176fa1bc4f951a5c678b5b626bd043cb5
Mateo Nieto <matheo.developer@gmail.com>
2021-10-13 22:14:56 -0500

    t2

--- app/screens/Cart.jsx
+++ app/screens/Cart.jsx
@@ -7,13 +7,12 @@ import { connect } from 'react-redux';
 import { FormatMoneyService } from '@core/services/format-money.service';
 import { ProductCart as ProductCartService } from '@core/services/product-cart.service';
 
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 import Tabs from '@custom-elements/Tabs';
 import ProductCart from '@custom-elements/ProductCart';
+import { AlertService } from '@core/services/alert.service';
+
 
 const { width } = Dimensions.get('screen');
 class Cart extends React.Component {
@@ -23,19 +22,37 @@ class Cart extends React.Component {
     this.state = {
       customStyleIndex: 0,
       deleteAction: false,
+      myPrice: false
     };
 
+    this.alertService = new AlertService();
     this.formatMoney = FormatMoneyService.getInstance();
     this.productCartService = ProductCartService.getInstance(props.cartProducts);
+  }
+
+  componentDidMount(){
+    this.setState({
+      myPrice: this.props.cartProducts[0].myPrice
+    })
   }
 
   componentDidUpdate(prevProps) {
     if (JSON.stringify(this.props.cartProducts) !== JSON.stringify(prevProps.cartProducts)) {
       this.productCartService = ProductCartService.getInstance(this.props.cartProducts);
+
+      this.setState({
+        myPrice: this.props.cartProducts[0].myPrice
+      })
     }
+
   }
 
   onCheckoutPressed() {
+
+    if(this.state.myPrice){
+      this.alertService.show('Alert!', 'Cannot checkout in client mode, please disable');
+      return;
+    }
     this.props.navigation.navigate('PlaceOrders');
   }
 
@@ -55,7 +72,7 @@ class Cart extends React.Component {
     );
   }
 
-  renderProducts = ({ item }) => <ProductCart product={item} myPrice={this.state.myPrice} />;
+  renderProducts = ({ item }) => <ProductCart product={item} />;
 
   renderCurrentOrder = () => (
     <FlatList
@@ -128,8 +145,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-between'
-
+    justifyContent: 'space-between',
   },
   container_empty: {
     height: hp('60%'),
