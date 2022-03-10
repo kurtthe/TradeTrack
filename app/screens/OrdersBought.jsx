a735511da9e29b809089a4479563659c79b428b7
Mateo Nieto <mnieto@technisys.com>
2022-03-09 22:23:58 -0500

    left add cart products

--- app/screens/OrdersBought.jsx
+++ app/screens/OrdersBought.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Block } from 'galio-framework';
-import { StyleSheet } from 'react-native';
+import { Block, Button, Text } from 'galio-framework';
+import { StyleSheet, Dimensions } from 'react-native';
 import ListCart from '@custom-sections/ListCart'
+import { nowTheme } from '@constants/index';
+
+const { width } = Dimensions.get("screen");
 
 const OrdersBought = ({ route }) => {
   const { products } = route.params
 
-  console.log("=0<products",products?.items)
+  console.log("=0<products", products?.items)
 
   return (
     <Block style={styles.container}>
       <ListCart cartProducts={products?.items} messageCartEmpty='No have products in this order' />
+      <Block style={styles.footer}>
+        <Button
+          shadowless
+          color={nowTheme.COLORS.INFO}
+          onPress={() => this.onCheckoutPressed()}
+        >
+          <Text size={18} color={nowTheme.COLORS.WHITE}>
+            Add to cart
+          </Text>
+        </Button>
+      </Block>
     </Block>
   )
 }
@@ -18,6 +32,10 @@ const OrdersBought = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
   }
 })
 
