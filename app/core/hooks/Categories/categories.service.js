69a451387fcb57c63fb87f9bd6e86d3af19dfba7
Jefferson Solano <solanojefferson@gmail.com>
2022-05-18 16:57:06 -0500

    try resolve error in categories but I couldn't yet

--- app/core/hooks/Categories/categories.service.js
+++ app/core/hooks/Categories/categories.service.js
@@ -4,6 +4,9 @@ import { endPoints } from '@shared/dictionaries/end-points';
 const generalRequestService = GeneralRequestService.getInstance()
 
 export const getCategories = async (options) => {
+
+  console.log("=>getCategories",options)
+  
   const response = await generalRequestService.getWithHeaders(endPoints.categories, {},
     {
       'per-page': 20,
