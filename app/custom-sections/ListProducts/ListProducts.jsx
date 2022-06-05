cd2191bf594d1447b3bba45724af11756af693dc
Mateo Nieto <mnieto@technisys.com>
2022-06-04 20:46:50 -0500

    filters

--- app/custom-sections/ListProducts/ListProducts.jsx
+++ app/custom-sections/ListProducts/ListProducts.jsx
@@ -3,36 +3,11 @@ import React, { useState } from 'react';
 import Filters from './components/Filters'
 import Products from './components/Products';
 
-const ListProducts = ({ categorySelected }) => {
-  const [productsFiltered, setProductsFiltered] = useState([])
-  const [pageProducts, setPagesProducts] = useState(1)
-
-  const handleChangeCategory = (dataProducts, reset = false) => {
-    if (reset) {
-      setProductsFiltered([])
-      setPagesProducts(1)
-      return
-    }
-    setProductsFiltered(dataProducts)
-  }
-
-  const handleLoadingMore = () => {
-    setPagesProducts(pageProducts + 1)
-  }
-
-  return (
-    <>
-      <Filters
-        categorySelected={categorySelected}
-        onSelectCategory={(products, reset) => handleChangeCategory(products, reset)}
-        pageProducts={pageProducts}
-      />
-      <Products
-        productsFiltered={productsFiltered}
-        onLoadingMore={handleLoadingMore}
-      />
-    </>
-  );
-};
+const ListProducts = () => (
+  <>
+    <Filters />
+    <Products />
+  </>
+);
 
 export default ListProducts;
