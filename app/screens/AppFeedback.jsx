94ceeb0827943021aaaa48431d7620fede51f5a6
Jefferson Solano <solanojefferson@gmail.com>
2022-07-12 01:11:07 -0400

    webview app feedback

---
+++ app/screens/AppFeedback.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import WebViewComponent from '@custom-elements/WebView';
+
+const AppFeedback = () => {
+  return (
+    <View style={styles.webViewContainer}>
+      <WebViewComponent url={`https://tradetrak.notion.site/Burdens-App-Portal-Feature-Requests-c7e641b310a34cf8a904e65fad508433`} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  webViewContainer: {
+    flex: 1,
+  },
+});
+
+export default AppFeedback;
