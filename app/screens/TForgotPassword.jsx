366219cc1504efbc74e30bd5f74179972a602fa6
Jefferson Solano <solanojefferson@gmail.com>
2022-07-13 20:01:05 -0400

    labels updated forgot password

--- app/screens/TForgotPassword.jsx
+++ app/screens/TForgotPassword.jsx
@@ -25,6 +25,8 @@ const DismissKeyboard = ({ children }) => (
   <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>{children}</TouchableWithoutFeedback>
 );
 
+
+
 class ChangePassword extends React.Component {
   constructor(props) {
     super(props);
@@ -35,6 +37,9 @@ class ChangePassword extends React.Component {
       hidePass: true,
     };
   }
+  handleChangeEmail = (text) => {
+    this.setState({ email: text });
+  };
   render() {
     const { navigation, scene } = this.props;
 
@@ -57,7 +62,7 @@ class ChangePassword extends React.Component {
                 <Block flex middle style={styles.socialConnect}>
 
 
-                  <Block flex={3} top middle style={{ top: (Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 30 : 30) : (Dimensions.get('window').height < 870) ? 30 : 40 }} >
+                  <Block flex={3} top middle style={{ top: (Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 30 : 0) : (Dimensions.get('window').height < 870) ? 30 : 40 }} >
                     <Image style={styles.introImageStyle} source={require('@assets/imgs/img/logo.png')} />
                   </Block>
                   <Block flex={3} top middle style={{ top: (Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 15 : 10) : (Dimensions.get('window').height < 870) ? 15 : 15 }}>
@@ -70,10 +75,10 @@ class ChangePassword extends React.Component {
                       size={(Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 20 : 22) : (Dimensions.get('window').height < 870) ? 20 : 26}
                     //size={20}
                     >
-                      Changes Password
+                    Forgot your current Password?
                     </Text>
                   </Block>
-                  <Block middle top flex={3} style={{ top: (Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 10 : -20) : (Dimensions.get('window').height < 870) ? 10 : -20 }} width={width * 0.9} >
+                  <Block middle top flex={8} style={{ top: (Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 10 : -20) : (Dimensions.get('window').height < 870) ? 10 : -20 }} width={width * 0.9} >
                     <Text
                       color={nowTheme.COLORS.PRETEXT}
                       style={{ fontFamily: 'montserrat-regular', }}
@@ -83,9 +88,7 @@ class ChangePassword extends React.Component {
                       size={(Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 18 : 20) : (Dimensions.get('window').height < 870) ? 18 : 20}
 
                     >
-
-
-                      Please fill in a maximum of 8-16 characters with numbers
+                      Enter the form to send you an email for changes the password
                     </Text>
                   </Block>
                 </Block>
@@ -96,62 +99,41 @@ class ChangePassword extends React.Component {
 
                       <Block>
 
-                        <Block flex={(Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 0.3 : 0.15) : (Dimensions.get('window').height < 870) ? 0.255 : 0.15} style={{
+                        <Block flex={(Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 0.3 : 0.5) : (Dimensions.get('window').height < 870) ? 0.255 : 0.15} style={{
                           marginTop:
                             (Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 45 : 0) : (Dimensions.get('window').height < 870) ? 42 : 0
                         }} >
-                          <Text
-                            color={nowTheme.COLORS.PRETEXT}
-                            style={{ marginLeft: 0, fontFamily: 'montserrat-regular', }}
-                            row
-                            muted
-                            size={15}
-                          >
-                            New Password
-                          </Text>
+                           <Text
+                          color={nowTheme.COLORS.PRETEXT}
+                          style={{ marginLeft: 0, fontFamily: 'montserrat-regular' }}
+                          row
+                          muted
+                          size={
+                            Platform.OS === 'ios'
+                              ? Dimensions.get('window').height < 670
+                                ? 16
+                                : 20
+                              : Dimensions.get('window').height < 870
+                                ? 16
+                                : 20
+                          }
+                        >
+                          Email
+                        </Text>
+                        <Input
+                          right
+                          placeholder="Enter your email here"
+                          iconContent={<Block />}
+                          shadowless
+                          keyboardType={'email-address'}
+                          onChangeText={(event) => this.handleChangeEmail(event)}
+                          autoCapitalize="none"
+                        />
                         </Block>
-                        <Block width={width * 0.9}>
-                          <Input
-                            iconContent={<Block />}
-                            placeholder="•••••••••••••"
-                            secureTextEntry={this.state.hidePass ? true : false}
-                          />
-                          <MaterialIcons
-                            style={styles.icon}
-                            name={this.state.hidePass ? "visibility" : "visibility-off"}
-                            size={20}
-                            color={nowTheme.COLORS.ICON}
-                            onPress={() =>
-                              this.setState({ hidePass: !this.state.hidePass })
-                            }
-                          />
-                        </Block>
-                        <Block flex={0.2} style={{ marginTop: 30 }} >
-                          <Text
-                            color={nowTheme.COLORS.PRETEXT}
-                            style={{ marginLeft: 0, fontFamily: 'montserrat-regular', fontFamily: 'montserrat-regular', }}
-                            row
-                            muted
-                            size={15}
-                          >
-                            Confirm Password
-                          </Text>
-                        </Block>
+                       
+                        
                         <Block width={width * 0.9} >
-                          <Input
-                            iconContent={<Block />}
-                            placeholder="•••••••••••••"
-                            secureTextEntry={this.state.hidePass ? true : false}
-                          />
-                          <MaterialIcons
-                            style={styles.icon}
-                            name={this.state.hidePass ? "visibility" : "visibility-off"}
-                            size={20}
-                            color={nowTheme.COLORS.ICON}
-                            onPress={() =>
-                              this.setState({ hidePass: !this.state.hidePass })
-                            }
-                          />
+                         
                         </Block>
                       </Block>
                       <Block flex={(Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 0.8 : 0.45) : (Dimensions.get('window').height < 870) ? 1 : 0.4} center style={{ bottom: 20 }} >
@@ -162,7 +144,7 @@ class ChangePassword extends React.Component {
                           onPress={() => navigation.navigate("PasswordBeenChange")}
 
                         >
-                          Changes Password
+                         Send Email
                         </Button>
 
                         <SimpleButton onPress={() => navigation.navigate("Help")}>  <Text style={{ textDecorationLine: 'underline', }}>Need Help?</Text></SimpleButton>
