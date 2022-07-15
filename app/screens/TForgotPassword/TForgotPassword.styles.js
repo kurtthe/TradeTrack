3670414f42ff0902f5deb4d1a243661906349c91
Mateo Nieto <mnieto@technisys.com>
2022-07-15 09:28:13 -0500

    already forgot password

--- app/screens/TForgotPassword/TForgotPassword.styles.js
+++ app/screens/TForgotPassword/TForgotPassword.styles.js
@@ -1,20 +1,23 @@
 import { StyleSheet, Dimensions, Platform } from 'react-native'
 import { theme } from 'galio-framework';
 import nowTheme from "@constants/Theme";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+const { width } = Dimensions.get("screen");
 
 export const makeStyles = () => StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingHorizontal: 10
+    paddingHorizontal: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-between'
   },
-  contentLogo: {
-
+  content: {
+    marginBottom: 32
   },
   introImageStyle: {
-    width: "29%",
-    resizeMode: 'contain',
+    width: 130,
+    height: 47,
+    resizeMode: 'stretch',
   },
   textTitle: {
     fontSize: (Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 20 : 22) : (Dimensions.get('window').height < 870) ? 20 : 26,
@@ -26,5 +29,44 @@ export const makeStyles = () => StyleSheet.create({
     color: nowTheme.COLORS.PRETEXT,
     fontSize: (Platform.OS === 'ios') ? ((Dimensions.get('window').height < 670) ? 18 : 20) : (Dimensions.get('window').height < 870) ? 18 : 20,
     fontFamily: 'montserrat-regular',
+    lineHeight: 22,
+    marginTop: 16
+  },
+  labelEmail: {
+    color: nowTheme.COLORS.PRETEXT,
+    marginLeft: 0,
+    fontFamily: 'montserrat-regular',
+    fontSize: 14
+  },
+
+  contentFooter: {
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  contentBody: {},
+  button: {
+    marginBottom: theme.SIZES.BASE,
+    width: width - theme.SIZES.BASE * 3,
+    fontFamily: 'montserrat-bold',
+    fontSize: 16,
+    backgroundColor: nowTheme.COLORS.INFO
+  },
+  textHelp: {
+    textDecorationLine: 'underline',
+    color: '#0E3A90',
+    fontFamily: 'montserrat-bold',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  contentHelp: {
+    marginVertical: 15
+  },
+  textToLogin: {
+    color: '#444857',
+    fontSize: 15,
+  },
+  contentBackLogin: {
+    flexDirection: 'row',
+    marginBottom: 36
   }
 });
