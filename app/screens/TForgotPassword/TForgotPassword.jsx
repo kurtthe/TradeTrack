76a27bccdebb2c4149a266bfa42495f7ee0b903e
Jefferson Solano <solanojefferson@gmail.com>
2022-07-15 09:28:13 -0500

    already forgot password

--- app/screens/TForgotPassword/TForgotPassword.jsx
+++ app/screens/TForgotPassword/TForgotPassword.jsx
@@ -1,44 +1,47 @@
 import React, { useState } from 'react';
 import {
   Image,
-  Dimensions,
   View,
-  Text
+  Text,
+  TouchableOpacity
 } from 'react-native';
 
-import { Button, Icon, Input, } from '@components';
-import ForgotButton from '@components/ForgotButton'
+import { Button, Input, } from '@components';
 import SimpleButton from '@components/SimpleButton'
 
-import nowTheme from "@constants/Theme";
 import Header from '@custom-sections/Header'
 
 import { GeneralRequestService } from '@core/services/general-request.service'
 import { endPoints } from '@shared/dictionaries/end-points';
 import { AlertService } from '@core/services/alert.service';
 import { makeStyles } from './TForgotPassword.styles'
+import { regex } from '@shared/dictionaries/regex'
 
-const { height, width } = Dimensions.get("screen");
 const generalRequestService = GeneralRequestService.getInstance()
 const alertService = new AlertService();
 
 export const TForgotPassword = ({ navigation }) => {
-  const [isEnabled, setIsEnabled] = useState(false)
   const [email, setEmail] = useState("")
   const styles = makeStyles()
 
-  const handleChangeEmail = (text) => {
-    this.setState({ email: text });
-  };
 
   const handleRecoverPassword = async () => {
-    if (!this.state.email) {
-      this.alertService.show('Alert!', 'The email is required.');
+    if (!email) {
+      alertService.show('Alert!', 'The email is required.');
       return
     }
 
-    await this.generalRequestService.post(endPoints.forgotPassword, { username: this.state.email })
-    navigation.navigate("PasswordBeenChange")
+    if (!regex.email.test(email)) {
+      alertService.show('Alert!', 'Email is not valid');
+      return
+    }
+
+    const res = await generalRequestService.post(endPoints.forgotPassword, { username: email })
+
+    if (res) {
+      navigation.navigate("PasswordBeenChange")
+      setEmail("")
+    }
   }
   return (
     <>
@@ -47,17 +50,54 @@ export const TForgotPassword = ({ navigation }) => {
         navigation={navigation}
       />
       <View style={styles.container}>
-        <View style={styles.contentLogo}>
-          <Image style={styles.introImageStyle} source={require('@assets/imgs/img/logo.png')} />
+        <View style={styles.contentBody} >
+          <View style={styles.content}>
+            <Image style={styles.introImageStyle} source={require('@assets/imgs/img/logo.png')} />
+          </View>
+
+          <View style={styles.content}>
+            <Text style={styles.textTitle} >
+              Forgot your current Password?
+            </Text>
+            <Text style={styles.subTitle} >
+              Enter the form to send you an email for changes the password
+            </Text>
+          </View>
+
+          <View style={styles.contentInput}>
+            <Text style={styles.labelEmail}>Email</Text>
+            <Input
+              right
+              placeholder="Enter your email here"
+              iconContent={<View />}
+              shadowless
+              keyboardType={'email-address'}
+              onChangeText={(text) => setEmail(text)}
+              autoCapitalize="none"
+              value={email}
+            />
+          </View>
         </View>
 
-        <View >
-          <Text style={styles.textTitle} >
-            Forgot your current Password?
-          </Text>
-          <Text style={styles.subTitle} >
-          Enter the form to send you an email for changes the password
-          </Text>
+        <View style={styles.contentFooter}>
+          <Button
+            style={styles.button}
+            onPress={handleRecoverPassword}
+          >
+            Send Email
+          </Button>
+
+          <TouchableOpacity style={styles.contentHelp} onPress={() => navigation.navigate("Help")}>
+            <Text style={styles.textHelp}>Need Help?</Text>
+          </TouchableOpacity>
+
+          <View style={styles.contentBackLogin}>
+            <Text style={styles.textToLogin}>Already remember your password?
+            </Text>
+            <SimpleButton onPress={() => navigation.navigate("Login")}
+            > Login</SimpleButton>
+
+          </View>
         </View>
       </View>
     </>
