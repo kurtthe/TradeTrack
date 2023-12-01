785bae8db67453f9cd43faf247e3a75fe7499b2b
Mateo Nieto <matheo.developer@gmail.com>
2023-12-01 17:03:15 -0500

    to fix error when the user logout clean the cart store

--- app/custom-sections/Header/components/Icons/SearchHome.jsx
+++ app/custom-sections/Header/components/Icons/SearchHome.jsx
@@ -3,9 +3,14 @@ import { TouchableOpacity, Keyboard } from 'react-native'
 import { makeStyles } from './styles'
 import { Ionicons } from '@expo/vector-icons';
 import * as SecureStore from 'expo-secure-store';
+import { useDispatch } from 'react-redux';
+import { clearProducts } from '@core/module/store/cart/cart';
 
 export const SearchHome = ({  style, navigation }) => {
   const styles = makeStyles()
+  const dispatch = useDispatch()
+
+
   return(
   <TouchableOpacity
     style={([styles.button, style], { zIndex: 300 })}
@@ -13,6 +18,7 @@ export const SearchHome = ({  style, navigation }) => {
       await SecureStore.deleteItemAsync('data_user');
       Keyboard.dismiss();
       navigation.navigate('Login');
+      dispatch(clearProducts())
     }}
   >
     <Ionicons name="log-out-outline" color={'#828489'} size={28} />
