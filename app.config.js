b9311b7eb6c73246d381386eb92ce43f79e5b2f0
Jefferson Solano <solanojefferson@gmail.com>
2024-05-08 14:38:08 -0500

    changessss 3

--- app.config.js
+++ app.config.js
@@ -1,30 +1,30 @@
 const { withExpoSecureStore } = require('@expo/config-plugins');
 
 module.exports = {
-  "expo": {
-    "name": "Burdens",
-    "slug": "-Trade-Track-",
-    "privacy": "public",
-    "platforms": [
+  expo: {
+    name: "Burdens",
+    slug: "-Trade-Track-",
+    privacy: "public",
+    platforms: [
       "ios",
       "android"
     ],
-    "version": "1.5.6",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "cover",
-      "backgroundColor": "#ffffff"
+    version: "1.5.7",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "cover",
+      backgroundColor: "#ffffff"
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0,
-      "url": "https://u.expo.dev/64a079a5-3b38-4879-9bb1-ee6192444fff"
+    updates: {
+      fallbackToCacheTimeout: 0,
+      url: "https://u.expo.dev/64a079a5-3b38-4879-9bb1-ee6192444fff"
     },
-    "assetBundlePatterns": [
+    assetBundlePatterns: [
       "**/*"
     ],
-    "plugins": [
+    plugins: [
       [
         "expo-secure-store",
         "expo-font"
@@ -44,30 +44,30 @@ module.exports = {
           }
       ]
     ],
-    "ios": {
-      "config": {
-        "usesNonExemptEncryption": false
+    ios: {
+      config: {
+        usesNonExemptEncryption: false
       },
-      "supportsTablet": true,
-      "bundleIdentifier": "com.trade.Burdens",
-      "buildNumber": "10",
-      "googleServicesFile": "./GoogleAnalytics/GoogleService-Info.plist",
-      "userInterfaceStyle": "light",
-      "infoPlist": {
-        "NSCameraUsageDescription": "Allow Burdens to access your camera to take photos of the products for any requirement in your purchase",
-        "NSPhotoLibraryUsageDescription": "Allow Burdens to access your photos to select photos and then send a requirement of your  purchase",
-        "UIUserInterfaceStyle": "Light"
+      supportsTablet: true,
+      bundleIdentifier: "com.trade.Burdens",
+      buildNumber: "10",
+      googleServicesFile: "./GoogleAnalytics/GoogleService-Info.plist",
+      userInterfaceStyle: "light",
+      infoPlist: {
+        NSCameraUsageDescription: "Allow Burdens to access your camera to take photos of the products for any requirement in your purchase",
+        NSPhotoLibraryUsageDescription: "Allow Burdens to access your photos to select photos and then send a requirement of your  purchase",
+        UIUserInterfaceStyle: "Light"
       }
     },
-    "android": {
-      "config": {
-        "networkSecurityConfig": "./NetworkSecurity/network_security_config.xml",
-        "googleMaps": {
-          "apiKey": "AIzaSyBo1S1NNk3JpmZaiPM_kZSq0yz0vVbf194"
+    android: {
+      config: {
+        networkSecurityConfig: "./NetworkSecurity/network_security_config.xml",
+        googleMaps: {
+          apiKey: "AIzaSyBo1S1NNk3JpmZaiPM_kZSq0yz0vVbf194"
         }
       },
-      "package": "com.tradetrak.Burdens",
-      "permissions": [
+      package: "com.tradetrak.Burdens",
+      permissions: [
         "android.permission.READ_EXTERNAL_STORAGE",
         "android.permission.WRITE_EXTERNAL_STORAGE",
         "android.permission.DOWNLOAD_WITHOUT_NOTIFICATION",
@@ -77,17 +77,17 @@ module.exports = {
         "android.permission.FOREGROUND_SERVICE",
         "android.permission.ACCESS_BACKGROUND_LOCATION"
       ],
-      "versionCode": 59,
-      "googleServicesFile": "./GoogleAnalytics/google-services.json"
+      versionCode: 59,
+      googleServicesFile: "./GoogleAnalytics/google-services.json"
     },
-    "description": "Coded by Digital Basis",
-    "extra": {
-      "eas": {
-        "projectId": "64a079a5-3b38-4879-9bb1-ee6192444fff"
+    description: "Coded by Digital Basis",
+    extra: {
+      eas: {
+        projectId: "64a079a5-3b38-4879-9bb1-ee6192444fff"
       }
     },
-    "runtimeVersion": {
-      "policy": "appVersion"
+    runtimeVersion: {
+      policy: "sdkVersion"
     }
   }
 }
