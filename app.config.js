3af53912204684bf22f3af84107a602c0fc6cf06
Mateo Nieto <matheo.developer@gmail.com>
2024-08-16 19:40:53 -0500

    new build

--- app.config.js
+++ app.config.js
@@ -1,93 +1,87 @@
 module.exports = {
   expo: {
-    name: "Burdens",
-    slug: "-Trade-Track-",
-    privacy: "public",
-    platforms: [
-      "ios",
-      "android"
-    ],
-    version: "1.7",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
+    name: 'Burdens',
+    slug: '-Trade-Track-',
+    privacy: 'public',
+    platforms: ['ios', 'android'],
+    version: '1.7',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
     splash: {
-      image: "./assets/splash.png",
-      resizeMode: "cover",
-      backgroundColor: "#ffffff"
+      image: './assets/splash.png',
+      resizeMode: 'cover',
+      backgroundColor: '#ffffff',
     },
     updates: {
       fallbackToCacheTimeout: 0,
-      url: "https://u.expo.dev/64a079a5-3b38-4879-9bb1-ee6192444fff"
+      url: 'https://u.expo.dev/64a079a5-3b38-4879-9bb1-ee6192444fff',
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    assetBundlePatterns: ['**/*'],
     plugins: [
-      "@config-plugins/react-native-blob-util",
-      "@config-plugins/react-native-pdf",
+      '@config-plugins/react-native-blob-util',
+      '@config-plugins/react-native-pdf',
+      ['expo-secure-store', 'expo-font'],
       [
-        "expo-secure-store",
-        "expo-font",
+        'expo-build-properties',
+        {
+          android: {
+            usesCleartextTraffic: true,
+            compileSdkVersion: 34,
+            targetSdkVersion: 34,
+            buildToolsVersion: '34.0.0',
+          },
+          ios: {
+            deploymentTarget: '14.0',
+          },
+        },
       ],
-      [
-          "expo-build-properties",
-          {
-            android: {
-              usesCleartextTraffic: true,
-              compileSdkVersion: 34,
-              targetSdkVersion: 34,
-              buildToolsVersion: "34.0.0"
-            },
-            ios: {
-              deploymentTarget: "14.0"
-            }
-          }
-      ],
-      "expo-media-library"
+      'expo-media-library',
     ],
     ios: {
       config: {
-        usesNonExemptEncryption: false
+        usesNonExemptEncryption: false,
       },
       supportsTablet: true,
-      bundleIdentifier: "com.trade.Burdens",
-      buildNumber: "2",
-      googleServicesFile: "./GoogleAnalytics/GoogleService-Info.plist",
-      userInterfaceStyle: "light",
+      bundleIdentifier: 'com.trade.Burdens',
+      buildNumber: '2',
+      googleServicesFile: './GoogleAnalytics/GoogleService-Info.plist',
+      userInterfaceStyle: 'light',
       infoPlist: {
-        NSCameraUsageDescription: "Allow Burdens to access your camera to take photos of the products for any requirement in your purchase",
-        NSPhotoLibraryUsageDescription: "Allow Burdens to access your photos to select photos and then send a requirement of your  purchase",
-        UIUserInterfaceStyle: "Light"
-      }
+        NSCameraUsageDescription:
+          'Allow Burdens to access your camera to take photos of the products for any requirement in your purchase',
+        NSPhotoLibraryUsageDescription:
+          'Allow Burdens to access your photos to select photos and then send a requirement of your  purchase',
+        UIUserInterfaceStyle: 'Light',
+      },
     },
     android: {
       config: {
-        networkSecurityConfig: "./NetworkSecurity/network_security_config.xml",
+        networkSecurityConfig: './NetworkSecurity/network_security_config.xml',
         googleMaps: {
-          apiKey: "AIzaSyBo1S1NNk3JpmZaiPM_kZSq0yz0vVbf194"
-        }
+          apiKey: 'AIzaSyBo1S1NNk3JpmZaiPM_kZSq0yz0vVbf194',
+        },
       },
-      package: "com.tradetrak.Burdens",
+      package: 'com.tradetrak.Burdens',
       permissions: [
-        "android.permission.READ_EXTERNAL_STORAGE",
-        "android.permission.WRITE_EXTERNAL_STORAGE",
-        "android.permission.DOWNLOAD_WITHOUT_NOTIFICATION",
-        "android.permission.ACCESS_NETWORK_STATE",
-        "android.permission.ACCESS_COARSE_LOCATION",
-        "android.permission.ACCESS_FINE_LOCATION",
-        "android.permission.FOREGROUND_SERVICE",
+        'android.permission.READ_EXTERNAL_STORAGE',
+        'android.permission.WRITE_EXTERNAL_STORAGE',
+        'android.permission.DOWNLOAD_WITHOUT_NOTIFICATION',
+        'android.permission.ACCESS_NETWORK_STATE',
+        'android.permission.ACCESS_COARSE_LOCATION',
+        'android.permission.ACCESS_FINE_LOCATION',
+        'android.permission.FOREGROUND_SERVICE',
       ],
-      versionCode: 64,
-      googleServicesFile: "./GoogleAnalytics/google-services.json"
+      versionCode: 65,
+      googleServicesFile: './GoogleAnalytics/google-services.json',
     },
-    description: "Coded by Digital Basis",
+    description: 'Coded by Digital Basis',
     extra: {
       eas: {
-        projectId: "64a079a5-3b38-4879-9bb1-ee6192444fff"
-      }
+        projectId: '64a079a5-3b38-4879-9bb1-ee6192444fff',
+      },
     },
     runtimeVersion: {
-      policy: "sdkVersion"
-    }
-  }
-}
+      policy: 'sdkVersion',
+    },
+  },
+};
