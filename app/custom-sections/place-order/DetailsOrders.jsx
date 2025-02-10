b3c34c5c726c7c5fa125263a3db395d89438101e
Mateo Nieto <matheo.developer@gmail.com>
2025-02-10 20:13:49 +0000

    disabled button checkout out

--- app/custom-sections/place-order/DetailsOrders.jsx
+++ app/custom-sections/place-order/DetailsOrders.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Dimensions, FlatList, Platform } from 'react-native';
-import { Block, Text, theme, Button } from 'galio-framework';
-import { nowTheme } from '@constants/index';
+import React, {useState} from 'react';
+import {StyleSheet, View, Dimensions, FlatList, Platform} from 'react-native';
+import {Block, Text, theme, Button} from 'galio-framework';
+import {nowTheme} from '@constants/index';
 import DetailOrder from '@custom-elements/DetailOrder';
-import { FormatMoneyService } from '@core/services/format-money.service';
+import {FormatMoneyService} from '@core/services/format-money.service';
 
-const { width } = Dimensions.get('screen');
+const {width} = Dimensions.get('screen');
 const formatMoney = FormatMoneyService.getInstance();
 
-const DetailsOrders = (props) => {
+const DetailsOrders = props => {
   const [loading, setLoading] = useState(false);
 
   const orderTotal = () => {
-    let prices = props.cartProducts.map((p) => {
+    let prices = props.cartProducts.map(p => {
       const price = p.myPrice ? p.rrp : p.cost_price;
       return price * p.quantity;
     });
@@ -26,26 +26,28 @@ const DetailsOrders = (props) => {
     setLoading(false);
   };
 
-  const renderOrder = ({ item }) => (
-    <DetailOrder order={item} />
-  )
-
+  const renderOrder = ({item}) => <DetailOrder order={item} />;
 
   return (
     <Block card backgroundColor={'white'} width={width}>
       <Block style={styles.detailOrdersBlock}>
-        <Text style={{ fontWeight: 'bold', fontSize: 16, paddingBottom: 15, paddingVertical: 25 }}>Detail Orders</Text>
-        <View style={{ flex: 1 }}>
-          {
-            props.cartProducts.length > 0 && (
-              <FlatList
-                data={props.cartProducts}
-                renderItem={renderOrder}
-                keyExtractor={(item, index) => `${item.id}-${index}`}
-
-              />
-            )
-          }
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontSize: 16,
+            paddingBottom: 15,
+            paddingVertical: 25,
+          }}>
+          Detail Orders
+        </Text>
+        <View style={{flex: 1}}>
+          {props.cartProducts.length > 0 && (
+            <FlatList
+              data={props.cartProducts}
+              renderItem={renderOrder}
+              keyExtractor={(item, index) => `${item.id}-${index}`}
+            />
+          )}
         </View>
         <View
           style={{
@@ -55,24 +57,32 @@ const DetailsOrders = (props) => {
             borderColor: '#E8E8E8',
           }}
         />
-        <Block row style={{ justifyContent: 'space-between', paddingBottom: 15, top: 10 }}>
+        <Block
+          row
+          style={{justifyContent: 'space-between', paddingBottom: 15, top: 10}}>
           <Text size={14}>Total (ex-GST)</Text>
           <Text
             size={16}
             color={nowTheme.COLORS.ORANGE}
-            style={{ fontWeight: Platform.OS == 'android' ? 'bold' : '600' }}
-          >
+            style={{fontWeight: Platform.OS == 'android' ? 'bold' : '600'}}>
             {orderTotal()}
           </Text>
         </Block>
-        <Block center style={{ position: 'relative', bottom: 5, paddingHorizontal: 20, paddingVertical: 14 }}>
+        <Block
+          center
+          style={{
+            position: 'relative',
+            bottom: 5,
+            paddingHorizontal: 20,
+            paddingVertical: 14,
+          }}>
           <Button
             color="info"
-            textStyle={{ fontFamily: 'montserrat-bold', fontSize: 16 }}
+            textStyle={{fontFamily: 'montserrat-bold', fontSize: 16}}
             style={styles.button}
             onPress={() => placeOrderHandler()}
             loading={loading}
-          >
+            disabled={loading}>
             Place Order
           </Button>
         </Block>
